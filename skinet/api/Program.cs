157fcb866e05f58995d0ff21e650a0f0cc25e805
AtanasLS <atanaslstoyanov.03@gmail.com>
2023-11-06 15:23:03 +0100

    Adding IProductRepo and ProductRepo and implementing it into the Controller.

--- skinet/api/Program.cs
+++ skinet/api/Program.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,6 +17,8 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
