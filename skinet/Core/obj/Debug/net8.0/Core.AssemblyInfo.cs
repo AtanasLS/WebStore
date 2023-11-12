369aac86776bad2ad5774792c2a4fa04da036046
AtanasLS <atanaslstoyanov.03@gmail.com>
2023-11-12 17:55:18 +0100

    Added consistent error response from the API

--- skinet/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ skinet/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2ad51e014f2f18f2b9b3ef799c83dc0742d9819")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+366d94f8dd8de675f66b0ce92afdbdffb75585c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
