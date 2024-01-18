d9ab3d65c5481232bc7333d045feb3ec94722df0
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-01-18 16:26:29 +0100

    Added simple nav-bar component

--- skinet/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ skinet/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92b19eb68801494f574b7181084d9c730537b7f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
