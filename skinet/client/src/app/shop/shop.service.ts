fff678df462bcd069ca5cdd6cf1bdef7b62d0eac
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-01-19 17:33:36 +0100

    Added the sort func and implemented it into the HTML

--- skinet/client/src/app/shop/shop.service.ts
+++ skinet/client/src/app/shop/shop.service.ts
@@ -13,12 +13,12 @@ baseUrl = 'https://localhost:5001/api/';
   constructor(private http: HttpClient) { }
 
 
-  getProducts(brandId?: number, typeId?: number){
+  getProducts(brandId?: number, typeId?: number, sort?: string){
     let params = new HttpParams();
 
     if(brandId) params = params.append('brandId', brandId);
     if(typeId) params = params.append('typeId', typeId);
-
+    if(sort) params = params.append('sort', sort);
 
     return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', {params})
   }
@@ -30,5 +30,6 @@ baseUrl = 'https://localhost:5001/api/';
   getTypes(){
     return this.http.get<Type[]>(this.baseUrl + 'products/types'); 
  }
+
 
 }
