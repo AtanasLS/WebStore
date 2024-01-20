cd77ec407bc2f65e878357eb610cd393011deaad
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-01-20 22:21:21 +0100

    Added the search func and implemnted it

--- skinet/client/src/app/shop/shop.service.ts
+++ skinet/client/src/app/shop/shop.service.ts
@@ -22,6 +22,8 @@ baseUrl = 'https://localhost:5001/api/';
     params = params.append('sort', shopParams.sort);
     params = params.append('pageIndex', shopParams.pageNumber);
     params = params.append('pageSize', shopParams.pageSize);
+    if(shopParams.search) params = params.append('search', shopParams.search);
+
 
     return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', {params})
   }
