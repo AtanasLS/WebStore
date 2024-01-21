b10d3387b2823a1e5ebe7217ff2dc10884930505
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-01-21 15:39:23 +0100

    added the func for getting an individual product and implemnted the basics of it

--- skinet/client/src/app/shop/shop.service.ts
+++ skinet/client/src/app/shop/shop.service.ts
@@ -14,6 +14,7 @@ baseUrl = 'https://localhost:5001/api/';
   constructor(private http: HttpClient) { }
 
 
+
   getProducts(shopParams: ShopParams){
     let params = new HttpParams();
 
@@ -26,6 +27,10 @@ baseUrl = 'https://localhost:5001/api/';
 
 
     return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', {params})
+  }
+
+  getProduct(id: number){
+    return this.http.get<Product>(this.baseUrl + 'products/' + id);
   }
 
   getBrands(){
