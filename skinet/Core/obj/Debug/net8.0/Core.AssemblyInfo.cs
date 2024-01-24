1b140f5a18ab6e4ae497e72a47c878316307d07f
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-01-24 14:24:23 +0100

    changed bootstrap theme and styling the product items

--- skinet/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ skinet/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0f6ab0dea72ddbabb8716ab3899efc3ff38b699")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66865463d9a3eb3304ebd431cb575c6e918fa00f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
