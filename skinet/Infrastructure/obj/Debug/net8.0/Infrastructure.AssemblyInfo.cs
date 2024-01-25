db454efaa3cce3eda7a4796bb982c6b30b715eeb
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-01-25 16:02:18 +0100

    Created the basket types and the basket service methods

--- skinet/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ skinet/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5529763f33989c8d4d21fa254c4e49428861500b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92ae81df42447fe8ea1a06cdacc9a2d8b68c4bb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
