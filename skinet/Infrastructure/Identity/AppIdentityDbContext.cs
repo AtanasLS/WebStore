6af513ce92193ce2a64adaaa8acb0a806e7225c9
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-01-27 20:52:49 +0100

    adding a new migration

--- skinet/Infrastructure/Identity/AppIdentityDbContext.cs
+++ skinet/Infrastructure/Identity/AppIdentityDbContext.cs
@@ -1,11 +1,12 @@
+using Core.Entities.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Infrastructure.Identity
+namespace Infrastructure.Data.Identity
 {
-    public class AppIdentityDbContex : IdentityDbContext
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
     {
-        public AppIdentityDbContex(DbContextOptions<AppIdentityDbContex> options) : base(options)
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
         {
         }
 
--- skinet/api/Extensions/IdentityServiceExtensions.cs
+++ skinet/api/Extensions/IdentityServiceExtensions.cs
@@ -1,22 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Infrastructure.Identity;
+using Infrastructure.Data.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.Extensions
 {
     public static class IdentityServiceExtensions
     {
-        public static IServiceCollection AddIdentityServices(this IServiceCollection services,
-         IConfiguration config)
-         {
-            services.AddDbContext<AppIdentityDbContex>(opt =>
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, 
+            IConfiguration config)
+        {
+            services.AddDbContext<AppIdentityDbContext>(opt =>
             {
                 opt.UseSqlite(config.GetConnectionString("IdentityConnection"));
             });
-
             return services;
          }
     }
