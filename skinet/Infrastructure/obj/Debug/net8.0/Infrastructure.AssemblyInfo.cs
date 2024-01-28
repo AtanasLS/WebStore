4db2949815c0a3c4dba287112537285b5226dca0
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-01-28 15:02:19 +0100

    added the register logic to the accController

--- skinet/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ skinet/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+931576349291c8e079ff5a9eff4d78cae0bd3003")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6871d3baa0dec8fec79a2a53880cb583c84baa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
