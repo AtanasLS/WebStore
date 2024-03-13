cbadd79b12edf62b676877a7d81d196d6d45b241
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-03-13 21:01:53 +0100

    added reusabel text validators component

--- skinet/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ skinet/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5b15be5634c8b71c5835352f6eac8266a6a9ec1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15efac3c0be69d28d69d6f34d22fbfc7dd008d8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
