00972880fd0a6f596f5906d2c11627223ec74bcc
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-03-16 14:43:12 +0100

    created the configurations for the order entities

--- skinet/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ skinet/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65a2ac6879d48f2812c58a23ecffffef46b80aad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8ea8f177d3c2dd02bade217f849f458b0348da8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
