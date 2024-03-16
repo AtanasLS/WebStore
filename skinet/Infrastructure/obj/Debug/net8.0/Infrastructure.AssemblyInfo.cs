70f52b4e5e208b787be29f790998e5ee71c81f1f
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-03-16 14:48:15 +0100

    added the delivery.json + Update the store context

--- skinet/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ skinet/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8ea8f177d3c2dd02bade217f849f458b0348da8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00972880fd0a6f596f5906d2c11627223ec74bcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
