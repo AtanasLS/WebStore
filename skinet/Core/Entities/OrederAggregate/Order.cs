b8ea8f177d3c2dd02bade217f849f458b0348da8
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-03-16 14:33:37 +0100

    Created the order entity

---
+++ skinet/Core/Entities/OrederAggregate/Order.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Entities.OrederAggregate
+{
+    public class Order : BaseEntity
+    {
+        public Order()
+        {
+        }
+
+        public Order(IReadOnlyList<OrderItem> orderItems, string bayerEmail, Address shipToAddress, 
+            DeliveryMethod deliveryMethod, decimal subtotal)
+        {
+            BayerEmail = bayerEmail;
+            ShipToAddress = shipToAddress;
+            DeliveryMethod = deliveryMethod;
+            OrderItems = orderItems;
+            Subtotal = subtotal;
+        }
+
+        public string BayerEmail { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+        public Address ShipToAddress { get; set; }
+        public DeliveryMethod DeliveryMethod { get; set; }
+        public IReadOnlyList<OrderItem> OrderItems { get; set; }
+        public decimal Subtotal { get; set; }
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+        public string PaymentIntentId { get; set; }
+
+        public decimal GetTotal() 
+        {
+            return Subtotal + DeliveryMethod.Price;
+        }
+
+    }
+}
--- skinet/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ skinet/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65a2ac6879d48f2812c58a23ecffffef46b80aad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8ccbe6073767971e2bc3b0eaa434f80d0299fd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
