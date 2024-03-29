f58a6c142953ac3a4fa5844083387619af7ba1df
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-03-29 21:48:18 +0100

    Added AutoMapper resolver for the OrderItem PictrureUrl

--- skinet/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ skinet/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8667c6942f4a838e5fec4b5fc6a7feffde15bab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ef83fdb131182964c8d955af3f4f0e211f0197c")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
