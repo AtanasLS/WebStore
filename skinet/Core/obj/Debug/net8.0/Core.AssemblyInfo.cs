2a22a94ccaf3e49f9070fb75f59e863559f1aec1
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-03-30 17:47:48 +0100

    setted up the stepper component

--- skinet/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ skinet/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ef83fdb131182964c8d955af3f4f0e211f0197c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f58a6c142953ac3a4fa5844083387619af7ba1df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
