740ace85d82f83ed14dba906696e15f54da8e818
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-03-30 18:11:20 +0100

    added the address form

--- skinet/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ skinet/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f58a6c142953ac3a4fa5844083387619af7ba1df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a22a94ccaf3e49f9070fb75f59e863559f1aec1")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
