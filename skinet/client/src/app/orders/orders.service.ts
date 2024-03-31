d765e7d96c3e9c20c17e60ce6dfad448f1e65347
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-03-31 14:54:16 +0200

    added the get orders method into the service

--- skinet/client/src/app/orders/orders.service.ts
+++ skinet/client/src/app/orders/orders.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environmet';
+import { Order } from '../shared/models/order';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrdersService {
 
-  constructor() { }
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getOrdersForUser() {
+    return this.http.get<Order[]>(this.baseUrl + 'order');
+  }
+
+  getOrderDatailed(id: number) {
+    return this.http.get<Order>(this.baseUrl + 'order/' + id);
+  }
 }
