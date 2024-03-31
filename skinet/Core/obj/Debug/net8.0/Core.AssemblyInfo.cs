02eed141ead2603d61bc1163f331807d709f8e68
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-03-31 14:45:56 +0200

    Added the routes for the orders

--- skinet/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ skinet/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+415e5ce605aaf785d6e973807ac15133c773d7a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+030cf7866b91766c29799904c85783ab035e2654")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- skinet/client/src/app/orders/orders-routing.module.ts
+++ skinet/client/src/app/orders/orders-routing.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OrdersModule } from './orders.module';
+import { OrdersComponent } from './orders.component';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { OrderDetailedComponent } from '../order-detailed/order-detailed.component';
 
-
+const routes: Routes = [
+  {path: '', component: OrdersComponent},
+  {path: ':id', component: OrderDetailedComponent, data: {breadcrumb: {alias: 'OrderDetailed'}}}
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    RouterModule
   ]
 })
+
 export class OrdersRoutingModule { }
