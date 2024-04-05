40de33ac9bf1146cf5d70a2e9267ec4a093a28de
AtanasLS <atanaslstoyanov.03@gmail.com>
2024-04-05 17:35:12 +0200

    made some adjustemnts into the shop-component.html and not ot load when deleting the basket after an order is made

--- skinet/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ skinet/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d27b6b36a7775a447088be579c64778bb458aa6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afe408a5e84120aa968705bbf649fbbb618b5a80")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
